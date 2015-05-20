23043a9817fe247a1e760159b610cbefac8938dc
Fraina <jscaem@gmail.com>
2015-05-20 18:02:51 +0800

    TODO: 個別設定預設 volume

--- ws-audio.js
+++ ws-audio.js
@@ -1,4 +1,3 @@
-// Web Audio API
 (function(factory){
     var id = 'WSAudio';
     if (typeof define === 'function' && define.amd) {
@@ -9,11 +8,10 @@
 })(function(){
   'use strict';
 
-  // TODO: this.o = {key: audio}
   var WSAudio = function() {
+    // pointer
     this.o = null;
-    this.sound = {};
-    this.audio = {};
+    this.trackList = {};
     this.playing = '';
     this.multiplay = false;
     this.setAttributes = function(el, attrs) {
@@ -26,32 +24,48 @@
   WSAudio.prototype = {
     init: function(params) {
       var sound = params.sounds || {},
-          attr = {
-            'id': 'WSAudioWrapper'
-          };
+          loopAll = params['loop'] || null,
+          preloadAll = params['preload'] || null,
+          defaultVolume = params['volume'] || 1,
+          autoplay = false;
 
-      if (typeof(params['loop']) != 'undefined' && params.loop) attr.loop = '';
-      if (typeof(params['autoplay']) != 'undefined' && params.autoplay) {
-        (Object.keys(sound).length === 1) ? attr.autoplay = '' : console.warn('Disable autoplay while there are multi sounds.');
+      if (params['autoplay'] === true) {
+        (Object.keys(sound).length === 1) ? autoplay = true : console.warn('Disable autoplay while there are multi sounds.');
       }
 
-      this.sound = sound;
+      for (var key in sound) {
+        var attr = {
+            'class': 'WSAudioWrapper',
+          };
+        if (autoplay) attr.autoplay = '';
+        if (loopAll != false && sound[key].loop != false) attr.loop = '';
+        if (preloadAll != false && sound[key].preload != false) attr.preload = '';
+
+        this.trackList[key] = {
+          'path': sound[key],
+          'audio': this.createAudioTag(sound[key], attr)
+        }
+      }
+
       this.multiplay = (typeof(params.multiplay) === 'boolean') ? params.multiplay : false;
-      this.createAudioTag(attr);
+      if (defaultVolume != 1) this.setVolume(defaultVolume);
 
       return this;
     },
 
-    createAudioTag: function(attr) {
+    createAudioTag: function(sound, attr, defaultVolume) {
       var WSAudioWrapper = document.createElement('audio');
       this.setAttributes(WSAudioWrapper, attr);
-      document.body.appendChild(WSAudioWrapper);
-      this.o = WSAudioWrapper;
+      if (WSAudioWrapper.canPlayType) {
+        this.createSourceTag(WSAudioWrapper, sound);
+      } else {
+        throw new Error('No audio support');
+      }
 
-      return this;
+      return WSAudioWrapper;
     },
 
-    createSourceTag: function(sound, appointType) {
+    createSourceTag: function(audio, sound, appointType) {
       var source = document.createElement('source'),
           attr = new Object(),
           audioSupported = {
@@ -63,69 +77,122 @@
 
       for (var key in audioSupported) {
         var type = appointType || key;
-        if (this.o.canPlayType(audioSupported[type]) && !Object.keys(attr).length) {
+        if (audio.canPlayType(audioSupported[type]) && !Object.keys(attr).length) {
           attr = {
-            'src': this.sound[sound] + '.' + type,
-            'type': audioSupported[type],
-            'id': 'soundSource'
+            'src': sound.path + '.' + type,
+            'type': audioSupported[type]
           }
           this.setAttributes(source, attr);
         }
       }
 
-      return this.o.appendChild(source);
+      return audio.appendChild(source);
     },
 
     play: function(sound, appointType) {
-      var canPlay = (! this.o.canPlayType) ? false : true;
-
-      if (! arguments.length) {
-        this.o.play();
-        return false;
-      }
-
-      if (canPlay) {
+      if (!arguments.length || sound === this.playing) {
+        if (this.o != null) this.o.play();
+      } else if (this.checkSoundValid(sound)) {
         if (! this.multiplay) {
-          this.o.innerHTML = '';
-          this.createSourceTag(sound, appointType);
-          this.playing = sound;
-          this.stop();
-          this.o.play();
+          if (this.o != null) this.stop();
         }
-      } else {
-        console.warn('No audio support');
+        // reflesh pointer
+        this.o = this.trackList[sound].audio;
+        this.trackList[sound].audio.play();
+        this.playing = sound;
       }
 
       return this;
     },
 
-    stop: function() {
-      this.o.load();
+    stop: function(sound) {
+      if (this.status() != null) {
+        (sound && this.checkSoundValid(sound)) ? this.trackList[sound].audio.load() : this.o.load();
+      }
+
       return this;
     },
 
-    pause: function() {
-      this.o.pause();
+    stopAll: function() {
+      for (var key in this.trackList) {
+        this.stop(key);
+      }
+
       return this;
     },
 
-    toggleMuted: function() {
-      this.o.muted = !this.o.muted;
+    pause: function(sound) {
+      if (this.status() != null) {
+        (sound && this.checkSoundValid(sound)) ? this.trackList[sound].audio.pause() : this.o.pause();
+      }
+
+      return this;
+    },
+
+    pauseAll: function() {
+      for (var key in this.trackList) {
+        this.pause(key);
+      }
+
+      return this;
+    },
+
+    toggleMuted: function(sound) {
+      if (this.status() != null) {
+        if (sound && this.checkSoundValid(sound)) {
+          var mutedStatus = this.trackList[sound].audio.muted;
+          mutedStatus = !mutedStatus;
+        } else {
+          this.o.muted = !this.o.muted;
+        }
+      }
+
+      return this;
+    },
+
+    setSoundVolume: function(sound, value) {
+      if (arguments.length === 2 && this.checkSoundValid(sound)) {
+        this.trackList[sound].audio.volume = arguments[1];
+      } else {
+        if (this.o != null) this.o.volume = arguments[0];
+      }
+
       return this;
     },
 
     setVolume: function(value) {
-      this.o.volume = value;
+      for (var key in this.trackList) {
+        this.setSoundVolume(key, value);
+      }
+
       return this;
     },
 
     status: function() {
-      var res = {
-        'playing': (this.o.currentTime) ? true : false,
-        'currentTime': this.o.currentTime,
-        'duration': this.o.duration,
-        'muted': this.o.muted
-      };
+      var res = null;
+      if (this.o != null) {
+        res = {
+          'isPlaying': (this.o.currentTime) ? true : false,
+          'sound': this.playing,
+          'currentTime': this.o.currentTime,
+          'duration': this.o.duration,
+          'volume': this.o.volume,
+          'muted': this.o.muted
+        };
+      }
+
+      return res;
+    },
+
+    checkSoundValid: function(sound) {
+      var res = false;
+      for (var key in this.trackList) {
+        if (key === sound) {
+          res = true;
+        }
+      }
+      if (!res) throw new Error('appointed track "' + sound + '" is invalid')
+
       return res;
     }
 
