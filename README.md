442f488c26962b25f354ca2311d5df4d81ec2204
Fraina <jscaem@gmail.com>
2015-05-21 16:28:06 +0800

    追加 API，補 markdown

--- README.md
+++ README.md
@@ -1,5 +1,73 @@
-Web Audio API
-
+WebSounder - Audio
 =========
 
 Play sounds with html5 audio tag.
+
+## Initialization ##
+Import plugin: ws-audio.js.
+```
+var audio = new WSAudio();
+audio.init({
+  'sounds': {
+    'track_alias': {
+      'file': 'track_fileName',
+      'type': 'ogg'
+    },
+    'soundA': {
+      'file': 'A_fileName',
+      'volume': 1,
+      'preload': false
+    },
+    'soundB': {
+      'file': 'B_fileName',
+      'loop': false
+    }
+  },
+  'path': 'path/',
+  'volume': 0.7,
+  'loop': true,
+  'autoplay': true,
+  'preload': true
+});
+```
+
+## Usage ##
+
+### Settings ###
+
+ - path (General)
+ - autoplay (General)
+ - preload (General, Sound object)
+ - volume (General, Sound object)
+ - loop (General, Sound object)
+ - type (General, Sound object)
+
+### API ###
+ - play
+```
+audio.play('soundA');
+audio.play('soundA', 'ogg');
+audio.play();   // play the last track
+```
+ - pause
+```
+audio.pause('soundA');
+audio.pause();   // pause the last played track
+```
+ - pauseAll
+ - stop
+```
+audio.stop('soundA');
+audio.stop();   // stop the last played track
+```
+ - stopAll
+ - toggleMuted
+ - setSoundVolume
+```
+audio.setSoundVolume('sound_A', 0.5);
+audio.setSoundVolume(0.5);   // change the last played track's volume to 0.5
+```
+ - setVolume
+
+   This method will influence all tracks, including those tracks were changed by `setSoundVolume`.
+ - status
--- ws-audio.js
+++ ws-audio.js
@@ -14,83 +14,45 @@
     this.trackList = {};
     this.playing = '';
     this.multiplay = false;
-    this.setAttributes = function(el, attrs) {
-      for(var key in attrs) {
-        el.setAttribute(key, attrs[key]);
-      }
-    }
   }
 
   WSAudio.prototype = {
     init: function(params) {
-      var sound = params.sounds || {},
-          loopAll = params['loop'] || null,
-          preloadAll = params['preload'] || null,
+      var soundData = params.sounds || {},
+          loopAll = params['loop'] || false,
+          preloadAll = params['preload'] || false,
           defaultVolume = params['volume'] || 1,
+          audioType = params['type'] || null,
+          path = params['path'] || '',
           autoplay = false;
 
       if (params['autoplay'] === true) {
-        (Object.keys(sound).length === 1) ? autoplay = true : console.warn('Disable autoplay while there are multi sounds.');
+        (Object.keys(soundData).length === 1) ? autoplay = true : console.warn('Disable autoplay while there are multi sounds.');
       }
 
-      for (var key in sound) {
-        var attr = {
-            'class': 'WSAudioWrapper',
-          };
+      this.formatPath = this.formatPath(path);
+      this.multiplay = (typeof(params.multiplay) === 'boolean') ? params.multiplay : false;
+
+      for (var key in soundData) {
+        var attr = {};
         if (autoplay) attr.autoplay = '';
-        if (loopAll != false && sound[key].loop != false) attr.loop = '';
-        if (preloadAll != false && sound[key].preload != false) attr.preload = '';
+        if (loopAll != false && soundData[key].loop != false) attr.loop = '';
+        if (preloadAll != false && soundData[key].preload != false) attr.preload = '';
+        if (soundData[key].volume >= 0 && soundData[key].volume <= 1) attr.volume = soundData[key].volume;
+        if (audioType != null || soundData[key].type) attr.type = soundData[key].type || audioType;
 
         this.trackList[key] = {
-          'path': sound[key],
-          'audio': this.createAudioTag(sound[key], attr)
+          'info': soundData[key],
+          'audio': this.createAudioTag(soundData[key], attr)
         }
       }
-
-      this.multiplay = (typeof(params.multiplay) === 'boolean') ? params.multiplay : false;
-      if (defaultVolume != 1) this.setVolume(defaultVolume);
+      this.applyDefaultVolume(defaultVolume);
 
       return this;
     },
 
-    createAudioTag: function(sound, attr, defaultVolume) {
-      var WSAudioWrapper = document.createElement('audio');
-      this.setAttributes(WSAudioWrapper, attr);
-      if (WSAudioWrapper.canPlayType) {
-        this.createSourceTag(WSAudioWrapper, sound);
-      } else {
-        throw new Error('No audio support');
-      }
-
-      return WSAudioWrapper;
-    },
-
-    createSourceTag: function(audio, sound, appointType) {
-      var source = document.createElement('source'),
-          attr = new Object(),
-          audioSupported = {
-            'mp3': 'audio/mpeg',
-            'ogg': 'audio/ogg;codecs="vorbis"',
-            'm4a': 'audio/mp4;codecs="mp4a.40.5"',
-            'wav': 'audio/wav'
-          };
-
-      for (var key in audioSupported) {
-        var type = appointType || key;
-        if (audio.canPlayType(audioSupported[type]) && !Object.keys(attr).length) {
-          attr = {
-            'src': sound.path + '.' + type,
-            'type': audioSupported[type]
-          }
-          this.setAttributes(source, attr);
-        }
-      }
-
-      return audio.appendChild(source);
-    },
-
     play: function(sound, appointType) {
-      if (!arguments.length || sound === this.playing) {
+      if (!arguments.length || (arguments.length === 1 && sound === this.playing)) {
         if (this.o != null) this.o.play();
       } else if (this.checkSoundValid(sound)) {
         if (! this.multiplay) {
@@ -98,8 +60,21 @@
         }
         // reflesh pointer
         this.o = this.trackList[sound].audio;
-        this.trackList[sound].audio.play();
         this.playing = sound;
+        switch (arguments.length) {
+          case 1:
+            this.o.play();
+            break;
+          case 2:
+            this.stop();
+            this.o.innerHTML = '';
+            this.createSourceTag(this.o, this.trackList[sound].info, arguments[1]);
+            this.o.load();
+            this.o.play();
+            break;
+          default:
+            return false;
+        }
       }
 
       return this;
@@ -137,13 +112,11 @@
       return this;
     },
 
-    toggleMuted: function(sound) {
+    toggleMuted: function() {
       if (this.status() != null) {
-        if (sound && this.checkSoundValid(sound)) {
-          var mutedStatus = this.trackList[sound].audio.muted;
-          mutedStatus = !mutedStatus;
-        } else {
-          this.o.muted = !this.o.muted;
+        var mutedStatus = this.o.muted;
+        for (var key in this.trackList) {
+          this.trackList[key].audio.muted = !mutedStatus
         }
       }
 
@@ -184,6 +157,8 @@
       return res;
     },
 
+    // helper method
+
     checkSoundValid: function(sound) {
       var res = false;
       for (var key in this.trackList) {
@@ -194,6 +169,63 @@
       if (!res) throw new Error('appointed track "' + sound + '" is invalid')
 
       return res;
+    },
+
+    createAudioTag: function(soundData, attr) {
+      var WSAudioWrapper = document.createElement('audio'),
+          appointType = attr.type || null;
+      this.setAttributes(WSAudioWrapper, attr);
+      if (WSAudioWrapper.canPlayType) {
+        this.createSourceTag(WSAudioWrapper, soundData, appointType);
+      } else {
+        throw new Error('No audio support');
+      }
+      return WSAudioWrapper;
+    },
+
+    createSourceTag: function(audio, soundData, appointType) {
+      var source = document.createElement('source'),
+          attr = new Object(),
+          audioSupported = {
+            'mp3': 'audio/mpeg',
+            'ogg': 'audio/ogg;codecs="vorbis"',
+            'm4a': 'audio/mp4;codecs="mp4a.40.5"',
+            'wav': 'audio/wav'
+          };
+
+      for (var key in audioSupported) {
+        var type = appointType || key;
+        if (audio.canPlayType(audioSupported[type]) && !Object.keys(attr).length) {
+          attr = {
+            'src': this.formatPath(soundData.file) + '.' + type,
+            'type': audioSupported[type]
+          }
+          this.setAttributes(source, attr);
+        }
+      }
+
+      return audio.appendChild(source);
+    },
+
+    setAttributes: function(el, attrs) {
+      for(var key in attrs) {
+        el.setAttribute(key, attrs[key]);
+      }
+    },
+
+    formatPath: function(path) {
+      return function(fileName) {
+        return path + fileName;
+      }
+    },
+
+    applyDefaultVolume: function(defaultVolume) {
+      for (var key in this.trackList) {
+        var volume = this.trackList[key].audio.getAttribute('volume');
+        this.setSoundVolume(key, (volume) ? volume : defaultVolume);
+      }
+
+      return this;
     }
 
   }
